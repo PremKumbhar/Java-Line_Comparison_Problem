9957bfbab007604ea8f87e48e4e1aff186cb2662
PremKumbhar <kumbharprem7738@gmail.com>
2022-10-11 18:37:28 +0530

    [Prem]:Added solve using OOPs

--- src/com/bridgelabz/LineComparison.java
+++ src/com/bridgelabz/LineComparison.java
@@ -3,53 +3,56 @@ package com.bridgelabz;
 import java.util.Scanner;
 
 public class LineComparison {
-	 public static int getLengthOfLine(Scanner scanner)	
-	 {
-         System.out.println("Please enter x1");
-         int x1 = scanner.nextInt();
-
-         System.out.println("Please enter y1");
-         int y1 = scanner.nextInt();
-
-         System.out.println("Please enter x2");
-         int x2 = scanner.nextInt();
-
-         System.out.println("Please enter y2");
-         int y2 = scanner.nextInt();
-
-         int val1 = (int) Math.pow((x2 - x1), 2);
-         int val2 = (int) Math.pow((y2 - y1), 2);
-         int result = (int) Math.sqrt(val1 + val2);
-
-         return result;
-      }
-
-
- public static void main(String[] args)
- 
-         {
-         Scanner scanner = new Scanner(System.in);
-         System.out.println("Please enter values for line 1");
-         int lenthOfline_1 = getLengthOfLine(scanner);
-         System.out.println("Please enter values for line 2");
-         int lenthOfline_2 = getLengthOfLine(scanner);
-        
-         int val1=lenthOfline_1;
-         int val2=lenthOfline_2;
-        
-         if(val1 == val2)
-         {
-                 System.out.println("Both lines are equal");
-         }
-         else if(val1 < val2)
-         {
-                 System.out.println("line 2 is greater");
-         }
-         else
-         {
-                 System.out.println("line 1 is greater");
-         }
-         
-         scanner.close();
-         }
+	public static int getLengthOfLine(Scanner scan) {
+		// User Input
+		  System.out.println("enter x1 point");
+	      int x1=scan.nextInt();
+	      
+	      System.out.println("enter y1 point");
+	      int y1=scan.nextInt();
+	      
+	      System.out.println("enter x2 point");
+	      int x2=scan.nextInt();
+	      
+	      System.out.println("enter y2 point");
+	      int y2=scan.nextInt();
+		
+	      int val1 = (int) Math.pow((x2 - x1), 2);
+	      int val2 = (int) Math.pow((y2 - y1), 2);
+	      int result = (int) Math.sqrt(val1 + val2);
+	      return result;
+	}
+	public void greatersmaller() 
+	{
+		// User Input
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Please enter value for line 1");
+		int lengthOfLine_1 = getLengthOfLine(scan);
+		System.out.println("Please enter value for line 2");
+		int lengthOfLine_2 = getLengthOfLine(scan);
+		
+		int val1 = lengthOfLine_1;
+		int val2 = lengthOfLine_2;
+		
+		System.out.println("*********************************************");
+		// To check greater than or less than
+		if (val1 == val2) {
+			System.out.println("Both lines are equal");
+		} else if (val1 < val2) {
+			System.out.println("Lines 2 is greater");
+		} else {
+			System.out.println("Lines 1 is greater");
+		}
+		scan.close();
+		
+	}
+	public static void main(String[] args) 
+	{
+		 System.out.println("Welcome to Line Comparison Program");
+		 
+		 // Creating an object
+		 LineComparison x = new LineComparison();
+		 // Calling methode
+		 x.greatersmaller();     
+	 }
 }
